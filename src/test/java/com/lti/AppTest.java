9036f4c49b1afae7e0ff6205e546fef415dceb67
vivitadias <diasvivita@gmail.com>
2020-11-21 16:54:45 +0530

    first commit

---
+++ src/test/java/com/lti/AppTest.java
@@ -0,0 +1,21 @@
+package com.lti;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+	System.out.println("Hello there");
+        assertTrue( true );
+    }
+}
